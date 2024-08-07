c87a8319a3bb1ba16c34a7c09558ed7723d7746d
Bibiana Garcia <bibiy.garcia@gmail.com>
2024-08-06 20:04:52 -1000

    Update 2

--- server/main.js
+++ server/main.js
@@ -1,16 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-}
-
-Meteor.startup(async () => {
-
-  //tasks collection initialization
+Meteor.startup(() => {
   if (TasksCollection.find().count() === 0) {
     [
       'First Task',
@@ -22,33 +15,4 @@ Meteor.startup(async () => {
       'Seventh Task'
     ].forEach(insertTask)
   }
-
-  // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://react-tutorial.meteor.com/simple-todos/01-creating-app.html',
-    });
-
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    });
-
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    });
-
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    });
-  }
-
-  // We publish the entire Links collection to all clients.
-  // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
-    return LinksCollection.find();
-  });
 });
